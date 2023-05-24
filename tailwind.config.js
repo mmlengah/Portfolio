58f383889f3b97b030d36aa3daaff83b921969ae
MatthiusM <98457199+mmlengah@users.noreply.github.com>
2023-05-24 14:32:54 +0100

    Finished about me section of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fontawesome': ['FontAwesome'],
+      },
+    },
   },
   plugins: [],
 }
