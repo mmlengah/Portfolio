1aba3a8164f31f693445f5a1aded16069fe46850
MatthiusM <98457199+mmlengah@users.noreply.github.com>
2023-05-26 15:04:15 +0100

    Setup links with express

--- app.js
+++ app.js
@@ -9,4 +9,24 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'projects.html'));
+});
+
+app.get('/dev-logs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'devlogs.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
